69109d111b2de5b9095854ea9351270c15199f6f
Alice (AristarkhovKI) <mortarion@gmail.com>
2021-03-01 15:23:25 +0300

    code: print the response

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-   cout << "Enter A and B: ";
    int a, b;
+   cout << "A + B = " << a + b << '\n'<< "A - B = " << a - b << '\n';
    cin >> a >> b;
     return 0;
 }
